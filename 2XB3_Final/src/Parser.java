379643e2fb97be76f89f8e2fc59ff21f91de851a
M. Dcruz <dcruzm@adsr.mcmaster.ca>
2016-04-07 17:33:23 -0400

    all

--- 2XB3_Final/src/Parser.java
+++ 2XB3_Final/src/Parser.java
@@ -1,6 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Parser {
-	public static void main (String[] args){
-		
+	public static void main(String[] args) {
+		try {
+
+			FileWriter fw = new FileWriter(new File("AmazonParsed.txt"),true);
+			BufferedWriter bw = new BufferedWriter(fw);
+			String line = "";
+			String line2 = "";
+			String line3 = "";
+			String line4 = "";
+			BufferedReader br = new BufferedReader(new FileReader(new File("Amazon.txt")));
+			while ((line = br.readLine()) != null) {
+				
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
